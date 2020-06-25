5b73328ea0e82045a70aab2c792da5474ceaf127
Ksenia Kozyreva <oestara91@gmail.com>
2020-06-26 01:14:54 +0300

    update manager branch

--- src/test/java/ru/netology/manager/ProductManagerBookTest.java
+++ src/test/java/ru/netology/manager/ProductManagerBookTest.java
@@ -4,7 +4,6 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import ru.netology.domain.Book;
 import ru.netology.domain.Product;
-import ru.netology.domain.Smartphone;
 import ru.netology.repository.ProductRepository;
 
 import static org.junit.jupiter.api.Assertions.*;
